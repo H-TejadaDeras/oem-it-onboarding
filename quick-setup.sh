948bf11bd4652d634001f888fb4849814c88d1ab
H-TejadaDeras <htejada@olin.edu>
2026-03-02 19:53:19 -0500

    Updated Quick Setup Script to v2.1; Added additional packages for Git and STM32 Firmware Related Packages Sections

--- quick-setup.sh
+++ quick-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # OEM IT Onboarding Quick Setup Script
-# v2.0 - 02-16-2026
+# v2.1 - 03-02-2026
 # 
 # Authors:
 # - Jack Greenberg - 09-24-2022
@@ -30,7 +30,7 @@ cl=$(printf '\033[0m')
 
 # Configuration Variables
 TMP_DIR=~/Downloads/oem-quick-setup-temp
-VERSION='v2.0 - 02-16-2026'
+VERSION='v2.1 - 03-02-2026'
 
 # Temporary Files for State Management (used to keep state across shell restarts)
 SILENT_MODE_FLAG="$TMP_DIR/silent_mode_flag.txt"
@@ -314,7 +314,7 @@ main () {
             if command -v git &> /dev/null; then # Check if Git is already installed
                 echo "Git is already installed. Skipping installation of Git."
             else
-                confirm_and_run "sudo apt install git-all -y"
+                confirm_and_run "sudo apt install git-all git-lfs -y"
             fi
             eval "git --version" # Verify Installation
 
@@ -377,7 +377,7 @@ main () {
             if [[ "$TOOLCHAIN" == "arm" || "$TOOLCHAIN" == "both" ]]; then
                 printf "\n"
                 printf "\n${green}${bold}Installing firmware related packages for STM32G441KB/STM32G441CB/STM32G474RE...${cl}\n"
-                confirm_and_run "sudo apt install openocd -y"
+                confirm_and_run "sudo apt install gdb-multiarch openocd -y"
             fi
 
             #########
